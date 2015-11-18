b742a2852de33b855dae097ee3ddb3bea3c01551
Jay4869 <jli4869@berkeley.edu>
2015-11-18 14:39:52 -0800

    update

--- pre-processing.py
+++ pre-processing.py
@@ -10,18 +10,19 @@ def pre_proccessing(fname, word):
     regx = re.compile('[%s]'%re.escape(string.punctuation))
     # loading each file
     for f in fname:
-        file = open(f, 'r')
+        files = open(f, 'r')
         # loading each line for oen file 
-        for i in file:
+        for i in files:
             # store all words in temp in 1D list
             temp = temp + regx.sub('', i.replace('\n', '').lower()).split(' ')
         # store all words in data in mult-dimssion splits by files
-        data.append([x for x in temp if x!= ''])
-        file.close()        
+        temp = [x for x in temp if x != '']
+        data.append(temp)
+        files.close()        
     # find the key of files
     key = list(set(temp))
-    for x in word:
-        key.remove(x)
+    # for x in word:
+    #     key.remove(x)
     # create a python dictionory and implement key and values
     freq = []
     for j in data:
@@ -30,20 +31,25 @@ def pre_proccessing(fname, word):
             tmp = tmp + [j.count(x)]
         freq.append(tmp)
     #save freq, list word and file name as csv files
-    with open('../../table.csv', 'w', newline='') as csvfile:
+    with open('table.csv', 'w', newline='') as csvfile:
         writerhead = csv.DictWriter(csvfile, fieldnames = key, delimiter=',', quotechar='', quoting=csv.QUOTE_NONE)
         writerhead.writeheader()
         writerfreq = csv.writer(csvfile, delimiter=',', quotechar='', quoting=csv.QUOTE_NONE)
         for row in freq:
             writerfreq.writerow(row)
-    csvfile.close()
+    # csvfile.close()
 
 if __name__ == '__main__':
-	from sys import argv
+    from sys import argv
 
-	filename = argv[1:]
-	word = []
-    file = open('common_word.txt', 'r')
-    for i in file:
-        word = word + i.replace(',',' ').split(' ')
-	table = pre_proccessing(filename, word)
+    word = []
+    filename = []
+    all_files = open('all_filenames.txt', 'r')
+    for i in all_files:
+        tmp = i.replace('\n', '')
+        filename = filename + [tmp[0:2] + 'data/' + tmp[2:]]
+    common_word = open('common_word.txt', 'r')
+    for j in common_word:
+        word = word + j.replace(',',' ').split(' ')
+    print(filename[6229:6231])
+    table = pre_proccessing(filename[6229:6231], word)
