618f3ffcb934344232124f782dc4701d4070b4af
Jay4869 <jli4869@berkeley.edu>
2015-11-21 11:33:01 -0800

    update and add history info

--- stat-154.R
+++ stat-154.R
@@ -39,6 +39,61 @@ dim(table)
 word.mat = table[,best_words]
 write.csv(word.mat, file="word_matrix.csv")
 
+#--------------------------------------------------------------------------------------
+filename = file.path("E:", "GitHub", "machine_learning", "Child(0)")
+dir(filename)
+setwd("E:/GitHub/machine_learning")
+
+common = scan('common_word.txt', what='', sep=',')
+common
+
+child = Corpus(DirSource(filename))
+child = tm_map(child, removeWords, common)
+
+child.weighted = DocumentTermMatrix(child,
+                                  control = list(weighting = function(x) weightTfIdf(x, normalize = TRUE),
+                                                 stopwords = TRUE, tolower = TRUE, removePunctuation = TRUE, 
+                                                 stemming=TRUE))
+childs = removeSparseTerms(child.weighted, 0.6)
+dim(childs)
+
+freq = colSums(as.matrix(childs))
+length(freq)
+
+a = boxplot(freq)
+best_words_child = names(freq[which(freq >= a$stats[2] & freq <= a$stats[4])]) #get middle of low and high bound
+length(best_words_child)
+
+#--------------------------------------------------------------------------------------
+filename = file.path("E:", "GitHub", "machine_learning", "History(1)")
+dir(filename)
+setwd("E:/GitHub/machine_learning")
+
+common = scan('common_word.txt', what='', sep=',')
+common
+
+history = Corpus(DirSource(filename))
+history = tm_map(history, removeWords, common)
+
+history.weighted = DocumentTermMatrix(history,
+                                    control = list(weighting = function(x) weightTfIdf(x, normalize = TRUE),
+                                                   stopwords = TRUE, tolower = TRUE, removePunctuation = TRUE, 
+                                                   stemming=TRUE))
+historys = removeSparseTerms(history.weighted, 0.6)
+dim(historys)
+
+freq = colSums(as.matrix(historys))
+length(freq)
+
+b = tail(sort(freq), 300)
+write.table(names(b), "history_300.txt", sep='\n')
+
+table = as.matrix(historys)
+dim(table)
+word.mat = table[,names(b)]
+write.csv(word.mat, file="history_300_matrix.csv")
+
+
 #freq.s = normalize.vector(freq)
 #CI = c(mean(freq.s) - sd(freq.s)/sqrt(3068), mean(freq.s) + sd(freq.s)/sqrt(3068))
 #best_word = names(freq.s)[which(freq.s >= CI[1] & freq.s <= CI[2])]
