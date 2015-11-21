8cc6cc4e7a1bbf048a4013d7fab6961e1a36e537
Jay4869 <jli4869@berkeley.edu>
2015-11-20 22:10:25 -0800

    update

--- stat-154.R
+++ stat-154.R
@@ -7,45 +7,36 @@ common
 
 library(NLP)
 library(tm)
-
-data = Corpus(DirSource(filename))
-data = tm_map(data, removePunctuation)
-data = tm_map(data, tolower)
-data = tm_map(data, removeWords, common)
-
 library(SnowballC)
-data = tm_map(data, stemDocument) 
-data = tm_map(data, stripWhitespace)
-data = tm_map(data, PlainTextDocument)
-
-dtm = DocumentTermMatrix(data)
-dtms = removeSparseTerms(dtm, 0.6)
-
-freq = colSums(as.matrix(dtms))
-freq
-#---------------------------------------------------
 library(NLP)
 library(tm)
 library(splines)
 library(MASS)
 library(ppls)
+
 data = Corpus(DirSource(filename))
 data = tm_map(data, removeWords, common)
 
-dtm.weighted <- DocumentTermMatrix(data,
+dtm.weighted2 <- DocumentTermMatrix(data,
                                    control = list(weighting = function(x) weightTfIdf(x, normalize = TRUE),
                                                   stopwords = TRUE, tolower = TRUE, removePunctuation = TRUE, 
-                                                  stemming=TRUE))
-dtms = removeSparseTerms(dtm.weighted, 0.95)
+                                                  stemming=TRUE, stripWhitespace = TRUE, PlainTextDocument = TRUE))
+dtms = removeSparseTerms(dtm.weighted2, 0.95)
 dim(dtms)
 
 freq = colSums(as.matrix(dtms))
 length(freq)
 
-freq.s1 = normalize.vector(freq)
-CI = c(mean(freq.s) - sd(freq.s), mean(freq.s) + sd(freq.s))
-best_word = names(freq.s)[which(freq.s >= CI[1] & freq.s <= CI[2])]
+a = boxplot(freq)
+best_words = names(freq[which(freq >= a$stats[2] & freq <= a$stats[4])])
+length(best_words)
 
-error = qnorm(0.84)*sd(freq)/sqrt(3068)
-best_word = names(freq)[which(freq >= mean(freq) - error & freq <= mean(freq) + error)]
-length(best_word)
+write.table(best_words, "best_words.txt", sep='\n')
+
+#freq.s = normalize.vector(freq)
+#CI = c(mean(freq.s) - sd(freq.s)/sqrt(3068), mean(freq.s) + sd(freq.s)/sqrt(3068))
+#best_word = names(freq.s)[which(freq.s >= CI[1] & freq.s <= CI[2])]
+
+#error = qnorm(0.998)*sd(freq)/sqrt(3068)
+#best_word = names(freq)[which(freq >= mean(freq) - error & freq <= mean(freq) + error)]
+#length(best_word)
